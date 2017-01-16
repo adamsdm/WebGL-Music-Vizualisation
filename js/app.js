8ac185fde86a544cb16dd602ba8f943c3f5f83c0
adam söderström <adam sderstrm>
2017-01-17 00:32:42 +0100

    Lots of changes again

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 var uniforms = {};
 uniforms.lfAmp = {};
 uniforms.hfAmp = {};
+var AudioController;
 
 window.onload = function () {
 
@@ -15,9 +16,9 @@ window.onload = function () {
         uniforms.hfAmp.value = AudioController.hfAmplitude;
         AudioController.draw();
     }
-
+    if (!init()) animate();
     AudioController.audio.play();
-    
+
 };
 
 
@@ -27,24 +28,28 @@ window.onload = function () {
 
 var stats, scene, renderer, composer, light;
 var camera, cameraControls;
-var pyramidMesh;
+var pyramidMesh, planetMaterial, particles, planetGeometry, bgMaterial;
+var stars = [];
 
 var time;
 var startTime = new Date().getTime();
 
 var PYRAMID_SCALE = 200;
 
-if (!init()) animate();
+
 
 // init the scene
-function init() {
+window.addEventListener( 'resize', onWindowResize, false );
 
+function init() {
+    
         initScene();
         postprocessing();
         addLights();
         addObjects();
 
         time = 0;
+
 }
 function initScene() {
 
@@ -77,11 +82,12 @@ function postprocessing() {
 
         // Film effect
         var noiseIntensity = 1.0;
-        var scanlinesIntensity = 1.0;
-        var scanlinesCount = 100;
+        var scanlinesIntensity = 0.7;
+        var scanlinesCount = 4;
         var grayscale = false;
 
-        var effectFilmBW = new THREE.FilmPass(noiseIntensity, scanlinesIntensity, scanlinesCount, grayscale);
+        var effectFilmLF = new THREE.FilmPass(noiseIntensity, scanlinesIntensity, scanlinesCount, grayscale);
+        var effectFilmHF = new THREE.FilmPass(0.9, scanlinesIntensity, 1024, grayscale);
 
         // RGB shift
         var colorShiftEffect = new THREE.ShaderPass(THREE.RGBShiftShader);
@@ -89,7 +95,8 @@ function postprocessing() {
         colorShiftEffect.renderToScreen = true;
 
         // Add effects
-        composer.addPass(effectFilmBW);
+        //omposer.addPass(effectFilmLF);
+        composer.addPass(effectFilmHF);
         composer.addPass(colorShiftEffect);
 
         // create a camera contol
@@ -103,6 +110,7 @@ function addLights() {
 
         light = new THREE.DirectionalLight(0xffffff);
         light.position.set(1000, 100, 100);
+        //light.position.set(0, 100, 600);
         scene.add(light);
 }
 
@@ -112,14 +120,14 @@ function addObjects() {
         var GROUND_DIM = 4000;
         var GROUND_SQUARES = 100;
 
-        var geometry = new THREE.PlaneGeometry(GROUND_DIM, GROUND_DIM, GROUND_SQUARES, GROUND_SQUARES);
+        var groundGeometry = new THREE.PlaneGeometry(GROUND_DIM, GROUND_DIM, GROUND_SQUARES, GROUND_SQUARES);
 
         uniforms.lightPos = { type: 'v3', value: light.position };
         uniforms.time = { type: 'v3', value: 1.0 };
         uniforms.lfAmp = { type: 'f', value: 0.0 };
         uniforms.hfAmp = { type: 'f', value: 0.0 };
 
-        // Create two uniform arrays to get different colors on two materials, ugly but works         
+        // Create two uniform arrays to get different colors on two materials, ugly but works
         var WFUniforms = {};
         var NoWFUniforms = {};
 
@@ -129,26 +137,28 @@ function addObjects() {
         });
 
         NoWFUniforms = Object.assign({}, uniforms, {
-                color1: { type: 'v3', value: [0.0, 0.0, 0.0] },
-                color2: { type: 'v3', value: [0.0, 0.0, 0.0] }
+                color1: { type: 'v3', value: [0.08, 0.13, 0.17] },
+                color2: { type: 'v3', value: [0.08, 0.13, 0.17] }
         });
 
         var groundMaterialWF = new THREE.ShaderMaterial({
                 uniforms: WFUniforms,
                 vertexShader: document.getElementById('vertexShader').textContent,
                 fragmentShader: document.getElementById('fragmentShader').textContent,
-                wireframe: true,
-                derivatives: true
+                wireframe: true
+
         });
         var groundMaterial = new THREE.ShaderMaterial({
                 uniforms: NoWFUniforms,
                 vertexShader: document.getElementById('vertexShader').textContent,
-                fragmentShader: document.getElementById('fragmentShader').textContent,
-                derivatives: true
+                fragmentShader: document.getElementById('fragmentShader').textContent
         });
 
+        groundMaterialWF.extensions.derivatives = true
+        groundMaterial.extensions.derivatives = true
+
         // TEMP
-        var planeMesh = THREE.SceneUtils.createMultiMaterialObject(geometry, [groundMaterialWF, groundMaterial]);
+        var planeMesh = THREE.SceneUtils.createMultiMaterialObject(groundGeometry, [groundMaterialWF, groundMaterial]);
 
         planeMesh.position.y = -60; //-1.9
         planeMesh.rotation.x = -Math.PI / 2;
@@ -162,9 +172,6 @@ function addObjects() {
         pyramidGeometry.faces = [new THREE.Face3(1, 0, 4), new THREE.Face3(2, 1, 4), new THREE.Face3(3, 2, 4), new THREE.Face3(0, 3, 4)];
 
 
-
-        console.log(pyramidGeometry.vertices);
-
         // Pyramid
         var geo = new THREE.EdgesGeometry(pyramidGeometry);
         var mat = new THREE.LineBasicMaterial({ color: 0xFF00FF, linewidth: 2 });
@@ -176,7 +183,34 @@ function addObjects() {
         pyramidMesh.position.set(0, -60, -1 * GROUND_DIM / 2);
         pyramidMesh.position.z -= Math.sqrt(2 * Math.pow(PYRAMID_SCALE, 2));
 
+
+
         //Background stars
+        var sprite = new THREE.TextureLoader().load( "images/ball.png" );
+        planetGeometry = new THREE.Geometry();
+				for ( i = 0; i < 300; i ++ ) {
+					var planetVertex = new THREE.Vector3();
+					planetVertex.x = (-1 + 2*Math.random())*1.4*window.innerWidth;
+					planetVertex.y = (-1 + 2*Math.random())*1.4*window.innerWidth;
+					planetVertex.z = -1 * GROUND_DIM / 2 - 600;
+					planetGeometry.vertices.push( planetVertex );
+				}
+				planetMaterial = new THREE.PointsMaterial( { size: 30, sizeAttenuation: false, map: sprite, alphaTest: 0.5, transparent: true } );
+				planetMaterial.color.setHSL( 1.0, 0.3, 0.7 );
+				particles = new THREE.Points( planetGeometry, planetMaterial );
+
+				scene.add( particles );
+
+        //BG
+        var bgGeometry = new THREE.PlaneGeometry(2*GROUND_DIM, 2*GROUND_DIM, GROUND_SQUARES, GROUND_SQUARES);
+        bgMaterial = new THREE.MeshBasicMaterial({
+            color: 0x123e84
+        });
+        var bgMesh = new THREE.Mesh( bgGeometry, bgMaterial );
+        bgMesh.position.z = -1 * GROUND_DIM / 2 -800;
+        scene.add(bgMesh)
+
+        //Text
         /** TODO **/
 }
 
@@ -186,22 +220,35 @@ function updateObjects() {
         pyramidMesh.rotation.y += 0.0002 * Math.pow(uniforms.lfAmp.value, 1.2);
         pyramidMesh.scale.y = PYRAMID_SCALE + 0.6 * uniforms.lfAmp.value;
 
-        //Song title
-        //var shadowDisp = -0.0005*uniforms.lfAmp.value;
-        //document.getElementById("songTitle").style.textShadow = String(shadowDisp)+"em "+ String(-shadowDisp) +"em 0em #FF00FF";
+        // Planets
+        var h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
+		planetMaterial.color.setHSL( h, 0.5, 0.5 );
+        planetMaterial.size = 5.0 + 0.6 * uniforms.lfAmp.value;
+        particles.rotation.z += 0.0001 * Math.pow(uniforms.lfAmp.value, 1.2)
+        particles.scale.y = 1 + 0.005 * uniforms.lfAmp.value;
+        particles.scale.x = 1 + 0.005 * uniforms.lfAmp.value;
+
+        // Background
+        bgMaterial.color.r = 0.01 + 0.5*uniforms.lfAmp.value/255;
+        bgMaterial.color.g = 0.04 + 0.2*uniforms.lfAmp.value/255;
+        bgMaterial.color.b = 0.08 + 0.2*uniforms.lfAmp.value/255;
+
+        // Camera
+        camera.position.y = 0.6 * uniforms.lfAmp.value;
+        camera.position.x = 20*Math.sin(time+0.05*uniforms.lfAmp.value);
 }
 
 // animation loop
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
 function animate() {
-
-        // loop on request animation loop
-        // - it has to be at the begining of the function
-        // - see details at http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
         requestAnimationFrame(animate);
-
         updateObjects();
-
-        // do the render
         render();
 }
 
@@ -215,13 +262,6 @@ function render() {
 
         // Update uniforms
         uniforms.time.value = time;
-
-        // animate PointLights
-        scene.traverse(function (object3d, idx) {
-                if (object3d instanceof THREE.PointLight === false) return;
-                var angle = 0.0005 * PIseconds * (idx % 2 ? 1 : -1) + idx * Math.PI / 3;
-                object3d.position.set(Math.cos(angle) * 3, Math.sin(angle * 3) * 2, Math.cos(angle * 2)).normalize().multiplyScalar(2);
-        });
 
         // actually render the scene
         composer.render(); //render with post processing
